bd49bfc04a336028e8da1a4a1089932a3864e260
pappu prasad <pappup@universal-sw.com>
2017-02-08 13:26:29 +0530

    CC-14161:Info: Required Regarding Black Duck Code

--- codecenter-sdk-examples/src/main/java/com/blackducksoftware/sdk/codecenter/client/examples/request/SampleSearchRequests.java
+++ codecenter-sdk-examples/src/main/java/com/blackducksoftware/sdk/codecenter/client/examples/request/SampleSearchRequests.java
@@ -1,17 +1,17 @@
 /*
  * Black Duck Software Suite SDK
  * Copyright (C) 2016 Black Duck Software, Inc.
- *
+ * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- *
+ * 
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
@@ -47,7 +47,6 @@ public class SampleSearchRequests extends SampleSearch<RequestSummary> {
         new SampleSearchRequests(args).run();
     }
 
-    // TODO: determine what this actually searches against
     @Argument(index = 3, required = false, metaVar = "searchString", usage = "part of the component name to search for; omit/make blank to retrieve all")
     protected String searchString;
 
